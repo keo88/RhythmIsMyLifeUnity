bfef589920fe4e2ed90693d2d77b9dc818db6336
keo88 <keo.kim0808@gmail.com>
2022-02-16 01:06:30 +0900

    2022-02-16 Improved performance by removing redundant music processing

--- Assets/Scripts/Chord.cs
+++ Assets/Scripts/Chord.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Chord
-{
-    public float offset;
-    public int pitch;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:79d41a1791bb7850362232c6ae14d38d9daf19d78ba497b7ee013b57220d8ff6
+size 182
---
+++ Assets/Scripts/SavedAudioInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6f62e08847ad1330def4e1a79953a1d1b2ce1469494ac14584400c8f4957571
+size 1813
