ee2ab42c699b6a138c2beb2a3603e902cccd57b2
SeungwooSon225 <sonseungw00123@gmail.com>
2022-04-06 21:12:20 +0900

    20220406 play random sound when player miscontroled

--- Assets/Scripts/MidiStreamManager.cs
+++ Assets/Scripts/MidiStreamManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffcc4fef77913cc87b97fc9c9ee7c2b532fcdeee73c3fe1ac213b1e223d643e1
-size 1978
+oid sha256:83bd5feeaa23cf232d7155173051967532ec287ec8fb57cdff1cc5ade51a6c1a
+size 3057
