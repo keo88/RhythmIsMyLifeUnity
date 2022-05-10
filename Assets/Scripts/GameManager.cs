96110ede3b6a87a31b1af5fe64fc85887fc965dc
SeungwooSon225 <sonseungw00123@gmail.com>
2022-05-10 14:11:09 +0900

    add game level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13df8bafb43dfd815894aaed2ec043fea363f0968feb17641c8389b788503d77
-size 13276
+oid sha256:b6361c5c980c99a1ee47793383ed362c4477f1aa69f4fb38d3f74c20f1620842
+size 13371
